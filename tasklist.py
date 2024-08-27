a99da22232849a2d997c0b9db678013e7e23defc
Mowsam Rai <raimowsam@gmail.com>
2024-08-27 11:12:45 +0100

    Add and Remove tasks from tasklists

--- tasklist.py
+++ tasklist.py
@@ -5,13 +5,20 @@ class TaskList:
     def __init__(self, tl_title, tl_id=None):
         self.tl_id = tl_id
         self.tl_title = tl_title
-        self.tasks = {}  # (key: task_id )
+        self.all_tasks = {}  # (key: task_id , value: Task())
 
     def add_task(self, task: Task):
-        return
+        if not isinstance(task, Task):
+            raise TypeError("The parameter must be a Task instance.")
+        self.all_tasks[task.id] = task
 
-    def add_task(self, task: Task):
-        return self.tasks.remove(task)
+    def remove_task(self, task: Task):
+        if not isinstance(task, Task):
+            raise TypeError("The parameter must be a Task instance.")
+        if task.id in self.all_tasks:
+            del self.all_tasks[task.id]
+        else:
+            raise KeyError(f"Task with ID {task.id} not found.")
 
     def list_all_tasks(self):
         '''
